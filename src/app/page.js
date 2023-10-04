22d57e43a7c7ae8b48e7021d71dc3c40f9274da8
Shubham Kharade <shubham.kharade28@gmail.com>
2023-10-04 03:40:48 -0700

    Fixed error finally

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,21 @@
 "use client";
-import {useRouter} from 'next/router';
+import Link from 'next/link';
+import {useRouter} from 'next/navigation';
+import { useEffect } from 'react';
 
-export default function Home() {
+const Home = () => {
 
   //if(!user) then push to page login 
   //if usertype = student then push to student dashboard
   //else if usertype = admin then push to admin dashboard
-  const router = useRouter();
-  router.push('/signin');
+  
+  
   return (
     <main className="container">
       <h1>Welcome to elective-allotment-system </h1>
+      <Link href="/signin">Go to signin &rArr;</Link>
     </main>
   )
 } 
+
+export default Home;
