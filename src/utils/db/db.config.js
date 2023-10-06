1c24e356ae5bdd236733c66cf6771dfa2690b88d
Shubham Kharade <shubham.kharade28@gmail.com>
2023-10-06 12:39:05 -0700

    Configured mongodb atlas

--- src/utils/db/db.config.js
+++ src/utils/db/db.config.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectionStr = process.env.connectionStr;
+// export const connectionStr = process.env.connectionStr;
+export const connectionStr = 'mongodb+srv://<shubhamk28>:<Shubham1234567>@cluster0.w5z6khr.mongodb.net/ele-allot?retryWrites=true&w=majority'
 
 const connectDB = async () => {
     await mongoose.connect(connectionStr);
